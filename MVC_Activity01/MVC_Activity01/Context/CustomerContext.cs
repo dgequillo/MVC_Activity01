e1a7ccc23170ad440ec2ccb2a7aacf5e8c223751
dgequillo <dgequillo@bpsolutions.biz>
2021-07-27 14:42:34 +0800

    name the table to Customer instead of Customers, or we can set length of Firstname to 64 characters only instead of Max.

--- MVC_Activity01/MVC_Activity01/Context/CustomerContext.cs
+++ MVC_Activity01/MVC_Activity01/Context/CustomerContext.cs
@@ -16,9 +16,19 @@ namespace MVC_Activity01.Context
             
         }
         public DbSet<Customers> Customers { get; set; }
+        public DbSet<CustomersDTO> CustomersDTO { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-           
+            modelBuilder.HasDefaultSchema("dbo");
+            modelBuilder.Entity<Customers>()
+                .ToTable("Customer");
+
+            modelBuilder.Entity<Customers>().HasKey(c => c.Id);
+            modelBuilder.Entity<Customers>().Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<Customers>().Property(c => c.Firstname).HasMaxLength(64);
+            modelBuilder.Entity<Customers>().Property(c => c.Middlename).HasMaxLength(64);
+            modelBuilder.Entity<Customers>().Property(c => c.Lastname).HasMaxLength(64);
+            modelBuilder.Entity<Customers>().Property(c => c.Gender).HasMaxLength(10);
         }
     }
 }
--- MVC_Activity01/MVC_Activity01/Controllers/CustomerController.cs
+++ MVC_Activity01/MVC_Activity01/Controllers/CustomerController.cs
@@ -11,6 +11,7 @@ namespace MVC_Activity01.Controllers
     {
         CustomerContext db = new CustomerContext();
         // GET: Customer
+
         public ActionResult Index()
         { 
             ViewBag.Title = "Customer List";
